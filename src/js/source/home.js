00b6eaf20572f89967ce297c17989d1b34426b2d
carola <carolacancinoc@gmail.com>
2016-12-29 13:47:01 -0300

    slider principal y nav listo

---
+++ src/js/source/home.js
@@ -0,0 +1,7 @@
+
+  $('.carousel.carousel-slider').carousel({full_width: true});
+
+   $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
