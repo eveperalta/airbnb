8002ebe56e40521ed3fe406e39c475c4e8f2ca77
EvePeralta <eperalta.cm@gmail.com>
2016-12-30 11:54:11 -0300

    cambios en date-picker arreglados con js y ademas en home.js agregando la parte de funcionalidad del carousel

--- src/js/source/home.js
+++ src/js/source/home.js
@@ -1,7 +1,11 @@
 
+  $(document).ready(function() {
+
   $('.carousel.carousel-slider').carousel({full_width: true});
 
    $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+
   });
