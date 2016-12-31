a2e13ef92eec602b9538fa149e8ff6815520e2f6
carola <carolacancinoc@gmail.com>
2016-12-31 06:12:47 -0300

    cambios home page

--- src/js/source/home.js
+++ src/js/source/home.js
@@ -1,7 +1,47 @@
-
+////SLIDER PRINCIPAL
   $('.carousel.carousel-slider').carousel({full_width: true});
 
+/////INPUT BUSCAR
    $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+
+   ////SLIDER DE 4
+ $('.owl-nome').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: true,
+    navText: [
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>"
+      ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+/////SLIDER DE 3
+   $(".classe").owlCarousel({
+      autoPlay: 5000, //Set AutoPlay to 3 seconds
+      items : 3,
+      navigation : true,
+      slideSpeed : 500,
+      paginationSpeed : 2000,
+      singleItem:false,
+      navigationText: [
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>"
+      ],
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
   });
